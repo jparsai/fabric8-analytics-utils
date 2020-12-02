c9d3f476a56336636d797f9189b892b60fdddbb6
Prashant Balachandran <pnair@redhat.com>
2020-12-02 17:01:30 +0530

    Adding default snyk encryption key (#81)
    Corrected lint error

--- f8a_utils/default_config.py
+++ f8a_utils/default_config.py
@@ -3,4 +3,5 @@ import os
 
 SNYK_API_TOKEN_VALIDATION_URL = os.getenv('SNYK_API_TOKEN_VALIDATION_URL',
                                           'https://snyk.io/api/v1/verify/token')
-ENCRYPTION_KEY_FOR_SNYK_TOKEN = os.getenv('ENCRYPTION_KEY_FOR_SNYK_TOKEN', 'SNYK')
+ENCRYPTION_KEY_FOR_SNYK_TOKEN = os.getenv('ENCRYPTION_KEY_FOR_SNYK_TOKEN',
+                                          'zXsuzlFTHnEDGRxsNMusmIMvXZ2Eg0DSJrXq8ounHP8')
