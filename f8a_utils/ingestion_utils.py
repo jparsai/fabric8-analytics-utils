25d6a25ee58cd0f6cc4087e9890e3ccf8304cf7c
jparsai <jparsai@redhat.com>
2020-12-21 18:38:00 +0530

    Utility function for triggering unknown package ingestion flow.

--- f8a_utils/ingestion_utils.py
+++ f8a_utils/ingestion_utils.py
@@ -1,3 +1,4 @@
+"""Utility file to trigger unknown package ingestion."""
 
 import logging
 import os
@@ -8,7 +9,7 @@ from requests_futures.sessions import FuturesSession
 
 logger = logging.getLogger(__name__)
 
-_APP_SECRET_KEY = os.getenv('APP_SECRET_KEY', 'not')
+_APP_SECRET_KEY = os.getenv('APP_SECRET_KEY', 'not-set')
 _INGESTION_API_URL = "http://{host}:{port}/{endpoint}".format(
     host=os.environ.get("INGESTION_SERVICE_HOST", "bayesian-jobs"),
     port=os.environ.get("INGESTION_SERVICE_PORT", "34000"),
@@ -16,30 +17,40 @@ _INGESTION_API_URL = "http://{host}:{port}/{endpoint}".format(
 _session = FuturesSession()
 
 
-def unknown_package_flow(ecosystem: str, unknown_pkgs: Set[namedtuple]) -> bool:
-    """Unknown Package flow. Trigger bayesianApiFlow."""
-    logger.debug('Triggered Unknown Package Flow for ecosystem: {} and Package: {}'
-                 .format(ecosystem, unknown_pkgs))
-    started_at = time.time()
-    # Create payload to be passed to ingestion API
-    payload = {
-        "ecosystem": ecosystem,
-        "packages": [],
-        "force": False,
-        "force_graph_sync": True
-    }
+def unknown_package_flow(ecosystem: str, unknown_pkgs: Set[namedtuple]):
+    """Unknown Package flow utility function.
 
-    # Set the unknown packages and versions
-    for pkg in unknown_pkgs:
-        payload['packages'].append({'package': pkg.name, 'version': pkg.version})
+    :param ecosystem: Ecosystem
+    :param unknown_pkgs: Set of tuple having packages name and version
+    """
+    try:
+        started_at = time.time()
+        logger.debug('Triggered Unknown Package Flow for ecosystem: {} and Package: {}'
+                     .format(ecosystem, unknown_pkgs))
 
-    if payload['packages']:
-        logger.info('Invoking Ingestion URL for payload = {}'.format(payload))
-        logger.info('_APP_SECRET_KEY = {}'.format(_APP_SECRET_KEY))
-        _session.post(url=_INGESTION_API_URL,
-                      json=payload,
-                      headers={'auth_token': _APP_SECRET_KEY})
+        # Create payload to be passed to ingestion API
+        payload = {
+            "ecosystem": ecosystem,
+            "packages": [],
+            "force": False,
+            "force_graph_sync": True
+        }
 
-    elapsed_time = time.time() - started_at
-    logger.info('Unknown flow for %f packages took %f seconds', len(unknown_pkgs), elapsed_time)
-    return True
+        # Set the unknown packages and versions
+        for pkg in unknown_pkgs:
+            payload['packages'].append({'package': pkg.name, 'version': pkg.version})
+
+        # If package list is not empty then call ingestion API
+        if payload['packages']:
+            logger.info('Invoking Ingestion URL for payload = {}'.format(payload))
+            _session.post(url=_INGESTION_API_URL,
+                          json=payload,
+                          headers={'auth_token': _APP_SECRET_KEY})
+
+        elapsed_time = time.time() - started_at
+        logger.info('Unknown flow for %f packages took %f seconds', len(unknown_pkgs), elapsed_time)
+        return True
+    except Exception as e:
+        logger.info('Failed to trigger unknown flow for payload {}'.format(payload))
+        logger.error(e)
+        return False
---
+++ tests/test_ingestion_utils.py
@@ -0,0 +1,29 @@
+"""Tests for classes from ingestion_utils module."""
+
+from f8a_utils.ingestion_utils import unknown_package_flow
+from unittest import mock
+from collections import namedtuple
+
+# Valid input data
+data_v1 = set()
+Package1 = namedtuple("Package", ["name", "version"])
+data_v1.add(Package1(name='pkg', version='ver'))
+
+# Invalid input data
+data_v2 = set()
+Package2 = namedtuple("Package", ["pkg", "ver"])
+data_v2.add(Package2(pkg='pkg', ver='ver'))
+
+
+@mock.patch('requests_futures.sessions.FuturesSession.post')
+def test_ingest_epv(_session):
+    """Test unknown_package_flow positive."""
+    res = unknown_package_flow('dummy_eco', data_v1)
+    assert res == True
+
+
+@mock.patch('requests_futures.sessions.FuturesSession.post')
+def test_ingest_epv_failed(_session):
+    """Test unknown_package_flow negative."""
+    res = unknown_package_flow('dummy_eco', data_v2)
+    assert res == False
