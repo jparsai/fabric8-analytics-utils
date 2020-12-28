64e83a7bad14113f1ecef8d7f9eccb37d10fdfa6
jparsai <jparsai@redhat.com>
2020-12-28 12:43:25 +0530

    fix: Add Unknown package ingestion functions (#83)
    * Utility function for triggering package unknown flow.
    
    * Utility function for triggering unknown package ingestion flow.
    
    * PyLint fix.
    
    * Error logging modified.
    
    * Modified function and test cases.
    
    * Modified try/except block and exception handling.
    
    * Added condition to accommodate changes done in keys by latest API server PR #710
    
    * Reverting changes done for keys.

---
+++ f8a_utils/ingestion_utils.py
@@ -0,0 +1,52 @@
+"""Utility file to trigger unknown package ingestion."""
+
+import logging
+import os
+from collections import namedtuple
+from typing import Set
+from requests_futures.sessions import FuturesSession
+
+logger = logging.getLogger(__name__)
+
+_APP_SECRET_KEY = os.getenv('APP_SECRET_KEY', 'not-set')
+_INGESTION_API_URL = "http://{host}:{port}/{endpoint}".format(
+    host=os.environ.get("INGESTION_SERVICE_HOST", "bayesian-jobs"),
+    port=os.environ.get("INGESTION_SERVICE_PORT", "34000"),
+    endpoint='ingestions/epv')
+_session = FuturesSession()
+
+
+def unknown_package_flow(ecosystem: str, unknown_pkgs: Set[namedtuple]):
+    """Unknown Package flow utility function.
+
+    :param ecosystem: Ecosystem
+    :param unknown_pkgs: Set of tuple having packages name and version
+    """
+    logger.debug('Triggered Unknown Package Flow for ecosystem: {} and Package: {}'
+                 .format(ecosystem, unknown_pkgs))
+
+    # Create payload to be passed to ingestion API
+    payload = {
+        "ecosystem": ecosystem,
+        "packages": [],
+        "force": False,
+        "force_graph_sync": True
+    }
+
+    # Set the unknown packages and versions
+    for pkg in unknown_pkgs:
+        # As API server and Backbone have different keys used.
+        payload['packages'].append({'package': pkg.package, 'version': pkg.version})
+
+    # If package list is not empty then call ingestion API
+    if payload['packages']:
+        try:
+            _session.post(url=_INGESTION_API_URL,
+                          json=payload,
+                          headers={'auth_token': _APP_SECRET_KEY})
+        except Exception as e:
+            logger.error('Failed to trigger unknown flow for payload %s with error %s',
+                         payload, e)
+            raise Exception('Ingestion failed') from e
+        else:
+            logger.info('Ingestion call being executed')
---
+++ tests/test_ingestion_utils.py
@@ -0,0 +1,27 @@
+"""Tests for classes from ingestion_utils module."""
+
+from f8a_utils.ingestion_utils import unknown_package_flow
+import unittest
+from unittest import mock
+from collections import namedtuple
+
+# Input data
+data_v1 = set()
+Package1 = namedtuple("Package", ["package", "version"])
+data_v1.add(Package1(package='pkg', version='ver'))
+
+
+class MyTestCase(unittest.TestCase):
+    """Test class for unknown_package_flow."""
+
+    @mock.patch('requests_futures.sessions.FuturesSession.post')
+    def test_ingest_epv(self, _session):
+        """Test unknown_package_flow positive."""
+        unknown_package_flow('dummy_eco', data_v1)
+
+    @mock.patch('requests_futures.sessions.FuturesSession.post')
+    def test_ingest_epv_failed(self, _session):
+        """Test unknown_package_flow negative."""
+        _session.side_effect = Exception(mock.Mock(return_value={'status': 404}), 'not found')
+        with self.assertRaises(Exception):
+            unknown_package_flow('dummy_eco', data_v1)
